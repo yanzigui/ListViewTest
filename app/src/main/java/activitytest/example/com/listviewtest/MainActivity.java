b364e85626155fdb1eea6528ee9ce01da4720d27
yzg <yzg@1.com>
2017-07-09 20:21:17 +0800

    点击事件

--- app/src/main/java/activitytest/example/com/listviewtest/MainActivity.java
+++ app/src/main/java/activitytest/example/com/listviewtest/MainActivity.java
@@ -2,8 +2,11 @@ package activitytest.example.com.listviewtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,8 +25,15 @@ public class MainActivity extends AppCompatActivity {
 
 //        ArrayAdapter<String>adapter = new ArrayAdapter<String>(
 //                MainActivity.this,android.R.layout.simple_list_item_1,data);
-        ListView listView = (ListView)findViewById(R.id.list_view);
+        ListView listView = (ListView) findViewById(R.id.list_view);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,int position,long id){
+                Fruit fruit = fruitList.get(position);
+                Toast.makeText(MainActivity.this,fruit.getName(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
     private void initFruits(){
         for(int i = 0;i<2;i++){
