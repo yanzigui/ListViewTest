75eff6add8066ed9aa84ce91ec0d6dc9d4fcb56b
yzg <yzg@1.com>
2017-07-09 19:38:49 +0800

    再优化

--- app/src/main/java/activitytest/example/com/listviewtest/FruitAdapter.java
+++ app/src/main/java/activitytest/example/com/listviewtest/FruitAdapter.java
@@ -25,16 +25,31 @@ public class FruitAdapter extends ArrayAdapter<Fruit>{
     public View getView(int position, View convertView, ViewGroup parent){
         Fruit fruit = getItem(position);//获得当前项的Fruit实例
         View view;
+        ViewHolder viewHolder;
         if(convertView == null){
             view = LayoutInflater.from(getContext()).inflate(resourceId,parent,false);
+            viewHolder = new ViewHolder();
+            viewHolder.fruitImage = (ImageView)view.findViewById(R.id.fruit_image);
+            viewHolder.fruitName = (TextView)view.findViewById(R.id.fruit_name);
+            view.setTag(viewHolder);//将ViewHolder存储在View中
+//            view = LayoutInflater.from(getContext()).inflate(resourceId,parent,false);
         }else{
             view = convertView;
+            viewHolder = (ViewHolder) view.getTag();//重新获取ViewHolder
         }
-//        View view = LayoutInflater.from(getContext()).inflate(resourceId,parent,false);
-        ImageView fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
-        TextView fruitName = (TextView) view.findViewById(R.id.fruit_name);
-        fruitImage.setImageResource(fruit.getImageId());
-        fruitName.setText(fruit.getName());
+        viewHolder.fruitImage.setImageResource(fruit.getImageId());
+        viewHolder.fruitName.setText(fruit.getName());
         return view;
+//        View view = LayoutInflater.from(getContext()).inflate(resourceId,parent,false);
+
+//        ImageView fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+//        TextView fruitName = (TextView) view.findViewById(R.id.fruit_name);
+//        fruitImage.setImageResource(fruit.getImageId());
+//        fruitName.setText(fruit.getName());
+//        return view;
+    }
+    class ViewHolder{
+        ImageView fruitImage;
+        TextView fruitName;
     }
 }
